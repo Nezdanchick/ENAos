fe62c808d061a6721c19623ceb10c417a39e4319
Nezdanchick <win10artem@gmail.com>
2024-04-06 03:31:05 +0400

    add data segment

--- src/x86_64/boot/gdt64.asm
+++ src/x86_64/boot/gdt64.asm
@@ -5,7 +5,9 @@ section .rodata
 gdt64:
 	dq 0	; entry
 .code_segment equ $ - gdt64
-	dq (1 << 43) | (1 << 44) | (1 << 47) | (1 << 53)	; code segment
+	dq (1 << 41) | (1 << 43) | (1 << 44) | (1 << 47) | (1 << 53)
+.data_segment equ $ - gdt64
+	dq (1 << 41) | (1 << 44) | (1 << 47)
 .pointer:
 	dw $ - gdt64 - 1
 	dq gdt64
