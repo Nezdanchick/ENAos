4a2cecc61b7472472ac55569600dd2b72c7779ef
Nezdanchick <win10artem@gmail.com>
2024-04-06 03:35:41 +0400

    add pc speaker

---
+++ src/include/pcspeaker.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <stdint.h>
+
+extern void play_sound(uint32_t frequence);
+extern void mute();
+extern void beep(int frequence, int length);
---
+++ src/x86_64/pcspeaker.c
@@ -0,0 +1,31 @@
+#include <pcspeaker.h>
+#include <asm.h>
+
+void play_sound(uint32_t frequence)
+{
+    if (frequence == 0)
+        return;
+
+    uint32_t div = 1193180 / frequence;
+    outb(0x43, 0xb6);
+    outb(0x42, (uint8_t)(div));
+    outb(0x42, (uint8_t)(div >> 8));
+
+    uint8_t tmp = inb(0x61);
+    if (tmp != (tmp | 3))
+    {
+        outb(0x61, tmp | 3);
+    }
+}
+void mute()
+{
+    uint8_t tmp = inb(0x61) & 0xFC;
+
+    outb(0x61, tmp);
+}
+void beep(int frequence, int length)
+{
+    play_sound(frequence);
+    // sleep(length); // TODO sleep(ms)
+    mute();
+}
