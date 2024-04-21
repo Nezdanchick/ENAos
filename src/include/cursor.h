6b619dfe6a21642e8a05cdc962970eb4cc9745fc
Nezdanchick <win10artem@gmail.com>
2024-04-21 13:33:30 +0400

    add cursor

---
+++ src/include/cursor.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <stdint.h>
+
+extern void enable_cursor();
+extern void disable_cursor();
+extern void update_cursor(int x, int y);
---
+++ src/x86_64/cursor.c
@@ -0,0 +1,25 @@
+#include <cursor.h>
+#include <asm.h>
+#include <screen.h>
+
+void enable_cursor()
+{
+    outb(0x3D4, 0x0B); // set the cursor end line to 15
+    outb(0x3D5, 0x0F);
+
+    outb(0x3D4, 0x0A); // set the cursor start line to 14 and enable cursor visibility
+    outb(0x3D5, 0x0E);
+}
+void disable_cursor()
+{
+    outb(0x3D4, 0x0A);
+    outb(0x3D5, 0x20);
+}
+void update_cursor(int x, int y)
+{
+    uint16_t pos = y * SCREEN_SIZE_X + x;
+    outb(0x3D4, 0x0F);
+    outb(0x3D5, (uint8_t)(pos & 0xFF));
+    outb(0x3D4, 0x0E);
+    outb(0x3D5, (uint8_t)((pos >> 8) & 0xFF));
+}
