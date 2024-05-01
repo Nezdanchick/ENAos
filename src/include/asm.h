2e93bf89322447653b57618ac1fb5b568f8d60db
Nezdanchick <win10artem@gmail.com>
2024-05-01 22:50:14 +0400

    add io functions for word and dword

--- src/include/asm.h
+++ src/include/asm.h
@@ -2,3 +2,7 @@
 
 extern void outb(uint16_t port, uint8_t value);
 extern uint8_t inb(uint16_t port);
+extern void outw(uint16_t port, uint16_t value);
+extern uint16_t inw(uint16_t port);
+extern void outl(uint16_t port, uint32_t value);
+extern uint32_t inl(uint16_t port);
--- src/x86_64/asm.c
+++ src/x86_64/asm.c
@@ -1,5 +1,6 @@
 #include <asm.h>
 
+// byte
 void outb(uint16_t port, uint8_t value)
 {
     asm("outb %0, %1" : : "a"(value), "Nd"(port));
@@ -8,5 +9,27 @@ uint8_t inb(uint16_t port)
 {
     uint8_t result;
     asm("inb %1, %0" : "=a"(result) : "Nd"(port));
+    return result;
+}
+// word
+void outw(uint16_t port, uint16_t value)
+{
+    asm("outw %0, %1" : : "a"(value), "Nd"(port));
+}
+uint16_t inw(uint16_t port)
+{
+    uint16_t result;
+    asm("inw %1, %0" : "=a"(result) : "Nd"(port));
+    return result;
+}
+// double word
+void outl(uint16_t port, uint32_t value)
+{
+    asm("outl %0, %1" : : "a"(value), "Nd"(port));
+}
+uint32_t inl(uint16_t port)
+{
+    uint32_t result;
+    asm("inl %1, %0" : "=a"(result) : "Nd"(port));
     return result;
 }
